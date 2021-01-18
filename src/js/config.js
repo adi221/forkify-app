604dc2cc70d5e2bfb908d995d9db2c362c3d7ba3
adi221 <adimizrahi421@gmail.com>
2021-01-18 10:34:03 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8ef242d5-dea0-4cae-a2d6-94c8ef4e34d8';
+export const MODAL_CLOSE_SEC = 2.5;
